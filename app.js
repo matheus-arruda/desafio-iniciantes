7f2bf2512f93df049cfb6ba2b3799523b8ecb22d
Hegnon <hegnondutra@gmail.com>
2022-11-26 14:03:03 -0300

    Criando rotas

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('hegnon')
+const { response } = require('express');
+var express = require('express');
+const { request } = require('http');
+var app = express();
+
+app.get('/api/inserir', function (req, res) {
+    res.send('Inserir dados');
+});
+
+app.get('/api/editar', function (req, res) {
+    res.send('Editar dados');
+});
+
+app.get('/api/atualizar', function (req, res) {
+    res.send('Atualizar dados');
+});
+
+
+
+app.listen(3000);
