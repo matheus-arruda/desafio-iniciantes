c9830dace2368852435478d6be97aefc60f29242
Hegnon Dutra <hegnondutra@gmail.com>
2022-12-11 16:56:30 -0300

    Trabalhando com as operações no banco de dados

--- src/routes/cliente.route.js
+++ src/routes/cliente.route.js
@@ -1,21 +1,20 @@
 var express = require ("express");
 var router = express.Router();
 
-router.get('/api/inserir', function (req, res) {
-    const cl=cliente.findAll();
+router.post('/api/inserir', function (req, res) {
     res.send('Inserir dados');
 });
 
-router.get('/api/editar', function (req, res) {
+router.put('/api/editar', function (req, res) {
     res.send('Editar dados');
 });
 
-router.get('/api/excluir', function (req, res) {
+router.delete('/api/excluir', function (req, res) {
     res.send('Excluir dados');
 });
 
-router.get('/api/atualizar', function (req, res) {
-    res.send('Atualizar dados');
+router.get('/api/obter', function (req, res) {
+    res.send('Obter dados');
 });
 
 
